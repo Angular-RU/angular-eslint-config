999ad39a9230f80ca26387b9fc5bc24694338b7e
splincode <omaxphp@yandex.ru>
2020-03-29 00:43:51 +0300

    fixup

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
-ts-node tools/publisher.ts --project tools/tsconfig.tools.json
-npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
-npm publish
+if [[ $TRAVIS_COMMIT_MESSAGE == *"only dependencies"* ]]; then
+  echo "Publish...."
+  ts-node tools/publisher.ts --project tools/tsconfig.tools.json
+  npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
+  npm publish
+fi;
