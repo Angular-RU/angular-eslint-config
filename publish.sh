81d020f2cf8ba3fbbecd2befc30422199932a641
splincode <omaxphp@yandex.ru>
2020-03-29 01:06:40 +0300

    trigger publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
-if [[ $TRAVIS_COMMIT_MESSAGE == *"only dependencies"* ]]; then
+#!/bin/bash
+
+echo "Commit message: $TRAVIS_COMMIT_MESSAGE";
+
+if [[ $TRAVIS_COMMIT_MESSAGE == *"only dependencies"* ||  $TRAVIS_COMMIT_MESSAGE == *"trigger publish"* ]]; then
   echo "Publish...."
   ts-node tools/publisher.ts --project tools/tsconfig.tools.json
   npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
